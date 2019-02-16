b343780adcccaabcf7bec9e4880c7e1b16378511
Wojcirej <wojciech.bal@epoczta.pl>
2019-02-16 13:12:43 +0100

    Enable pgcrypto extension.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_08_104024) do
+ActiveRecord::Schema.define(version: 2019_02_15_194517) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "employees", force: :cascade do |t|
