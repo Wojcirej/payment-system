81a9e0b6b5e34de899adbfbe6d552d285510b7e8
Wojcirej <wojciech.bal@epoczta.pl>
2019-02-24 20:12:28 +0100

    Add more integration tests.

--- spec/features/employees/index_spec.rb
+++ spec/features/employees/index_spec.rb
@@ -6,7 +6,7 @@ def open_employees_list_page
 end
 
 def navigate_to_second_page
-  page.all('a').to_a.find { |x| x.text == "2" }.trigger(:click)
+  page.find('a', text: "2").trigger(:click)
 end
 
 feature "Employee's list page", js: true do
@@ -18,6 +18,29 @@ feature "Employee's list page", js: true do
       expect(page).to have_content("Items per page")
       select_options = page.find('select').all('option').map { |x| x.value.split(": ").last }
       expect(select_options).to match_array(["5", "10", "25", "50", "100"])
+    end
+  end
+
+  scenario "Displays 25 elements on page by default" do
+    open_employees_list_page
+    expect(page).to have_selector("paginator-per-page-selector")
+    within("paginator-per-page-selector") do
+      expect(page).to have_content("Items per page")
+    end
+    within('tbody') do
+      expect(page).to have_selector('tr', count: 25)
+    end
+  end
+
+  scenario "Allows to change items per page" do
+    open_employees_list_page
+    expect(page).to have_selector("paginator-per-page-selector")
+    within("paginator-per-page-selector") do
+      expect(page).to have_content("Items per page")
+      page.find('select').find(:option, "10").select_option
+    end
+    within('tbody') do
+      expect(page).to have_selector('tr', count: 10)
     end
   end
 
@@ -34,7 +57,9 @@ feature "Employee's list page", js: true do
 
     scenario "Displays first page by default" do
       open_employees_list_page
+      expect(page).to have_selector("paginator-pages-selector")
       within("paginator-pages-selector") do
+        expect(page).to have_selector('a.active')
         active_page_button = page.find('a.active')
         expect(active_page_button.text).to eq("1")
       end
@@ -42,6 +67,7 @@ feature "Employee's list page", js: true do
 
     scenario "Does not display button navigating to previous page by default" do
       open_employees_list_page
+      expect(page).to have_selector("paginator-pages-selector")
       within("paginator-pages-selector") do
         visible_buttons_labels = page.all('a').map { |button| button.text }
         expect(visible_buttons_labels).not_to include("«")
@@ -52,6 +78,7 @@ feature "Employee's list page", js: true do
 
       scenario "Displays button allowing to navigate to the next page" do
         open_employees_list_page
+        expect(page).to have_selector("paginator-pages-selector")
         within("paginator-pages-selector") do
           next_page_button = page.all('a').last
           expect(next_page_button.text).to eq("»")
@@ -60,6 +87,7 @@ feature "Employee's list page", js: true do
 
       scenario "Can navigate to the other pages" do
         open_employees_list_page
+        expect(page).to have_selector("paginator-pages-selector")
         within("paginator-pages-selector") do
           navigate_to_second_page
           active_page_button = page.find('a.active')
@@ -69,6 +97,7 @@ feature "Employee's list page", js: true do
 
       scenario "After navigating off the first page, button navigating to previous page is displayed" do
         open_employees_list_page
+        expect(page).to have_selector("paginator-pages-selector")
         within("paginator-pages-selector") do
           navigate_to_second_page
           previous_page_button = page.all('a').first
